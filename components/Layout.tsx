55732fe2dc975a09cccd20bdbe8b0d30ca19f8f3
Dave Sanders <davepaulsanders@gmail.com>
2023-01-11 17:28:59 -0500

    Adding image and spacing

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -2,13 +2,13 @@ import Nav from "./Nav"
 import styles from "../styles/Layout.module.css"
 const Layout = ({children}: React.PropsWithChildren) => {
   return (
-        <div className="grid grid-cols-12 h-screen">
+        <div className="grid grid-cols-12 min-h-screen max-h-screen">
           <div className={`${styles.background} side-nav col-span-2 flex text-center justify-center items-center`}>
             <Nav />
           </div>
-        <div className="col-span-10">
-            <main className="">{children}</main>
-        </div>
+          <div className="col-span-10">
+            <main className="flex justify-center">{children}</main>
+          </div>
         </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head"
-
+import styles from "../styles/Index.module.css"
 export default function Home() {
   return (
     <>
@@ -9,7 +9,13 @@ export default function Home() {
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
         <link href="https://fonts.googleapis.com/css2?family=Hanken+Grotesk:wght@200;400;600;700&display=swap" rel="stylesheet"></link>    
     </Head>
-      <h1 className="text-white col-start-1 col-span-12 text-center">Hi there!</h1>
+    <div className="flex h-screen items-center w-3/4 pb-20">
+      <img className={`${styles.headshot} m-3 w-[300px]`} src="headshot.jpeg" alt="Dave Sanders" />
+      <div className="m-3">
+        <h1 className="text-6xl font-bold mb-3">Welcome!</h1>
+        <p>I&apos;m Dave Sanders, a web developer based out of Washington D.C.  Thanks for visiting!  </p>
+      </div>
+    </div>
     </>
   )
 }
