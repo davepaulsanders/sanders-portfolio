435404ba2030e5ef5842af165757f8e7b49b6773
Dave Sanders <davepaulsanders@gmail.com>
2023-01-12 16:15:15 -0500

    Hamburger menu and animations added to nav, typescript adjusted query selectors

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,16 +1,63 @@
-import Nav from "./Nav"
+import Nav from "./Nav";
+import styles from "../styles/Layout.module.css";
 
-const Layout = ({children}: React.PropsWithChildren) => {
+const Layout = ({ children }: React.PropsWithChildren) => {
+
+  const openMenuAnimation = () => {
+    const nav = document.querySelector(".nav") as HTMLElement;
+    const barTop = document.querySelector(".barTop") as HTMLElement;
+    const barMiddle = document.querySelector(".barMiddle") as HTMLElement;
+    const barBottom = document.querySelector(".barBottom") as HTMLElement;
+    nav.style.right = "0";
+    barTop.style.transform = "rotate(45deg)";
+    barMiddle.style.transform = "translateX(1rem)";
+    barMiddle.style.opacity = "0";
+    barBottom.style.transform = "rotate(-45deg)";
+  };
+
+  const closeMenuAnimation = () => {
+    const nav = document.querySelector(".nav") as HTMLElement;
+    const barTop = document.querySelector(".barTop") as HTMLElement;
+    const barMiddle = document.querySelector(".barMiddle") as HTMLElement;
+    const barBottom = document.querySelector(".barBottom") as HTMLElement;
+    nav.style.right = "-100%";
+    barTop.style.transform = "";
+    barMiddle.style.transform = "";
+    barMiddle.style.opacity = "";
+    barBottom.style.transform = "";
+  };
+
+  // toggle nav menu in mobile
+  const menuToggle = (e: React.MouseEvent) => {
+    e.preventDefault();
+    const nav = document.querySelector(".nav") as HTMLElement;
+    if (nav.classList.contains("open")) {
+      closeMenuAnimation();
+      nav.classList.remove("open");
+    } else {
+      openMenuAnimation();
+      nav.classList.add("nav-transition");
+      nav.classList.add("open");
+    }
+  };
   return (
     <div className="flex">
       <Nav />
-        <div className="grid grid-cols-12 min-h-screen max-h-screen">
-          <div className="col-span-12">
-            <main className="flex justify-center">{children}</main>
-          </div>
+      <div className="grid grid-cols-12 min-h-screen max-h-screen">
+        <div
+          className={`${styles.hamburgerMenu} flex md:hidden`}
+          onClick={menuToggle}
+        >
+          <div className={`${styles.barTop} barTop`}></div>
+          <div className={`${styles.barMiddle} barMiddle`}></div>
+          <div className={`${styles.barBottom} barBottom`}></div>
         </div>
+        <div className="col-span-12 lg:col-span-9 lg:col-start-4">
+          <main className="flex justify-center">{children}</main>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
 export default Layout;
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,18 @@ import Head from "next/head"
 import styles from "../styles/Index.module.css"
 
 export default function Home() {
+
+  
   return (
     <>
     <Head>
         <title>Dave Sanders Portfolio</title>
     </Head>
-    <div className="flex h-screen items-center w-3/4 pb-20">
+    <div className="flex flex-col md:flex-row h-screen items-center w-10/12 md:w-3/4 pt-20 md:pt-0 pb-20">
       <img className={`${styles.headshot} m-3 w-[300px]`} src="headshot.jpeg" alt="Dave Sanders" />
       <div className="m-3">
-        <h1 className="text-6xl font-bold mb-3">Welcome!</h1>
-        <p className="text-xl font-extralight" >I&apos;m Dave Sanders, a <span className={styles.span}>web developer</span> and <span className={styles.span}>musician</span> based out of Washington D.C.  Thanks for visiting!  </p>
+        <h1 className="text-5xl md:text-6xl text-center md:text-left font-bold mb-3">Welcome!</h1>
+        <p className="text-lg md:text-xl text-center md:text-left font-extralight" >I&apos;m Dave Sanders, a <span className={styles.span}>web developer</span> and <span className={styles.span}>musician</span> based out of Washington D.C.  Thanks for visiting!  </p>
       </div>
     </div>
     </>
